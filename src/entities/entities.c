f86edcbaf8f73481848eece5fb4d9e62b0109c12
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-05-16 23:30:07 -0300

    feat: creating Entity Lifetime functions

--- src/entities/entities.c
+++ src/entities/entities.c
@@ -5,8 +5,24 @@ static uint32_t g_entityFreelistCount;
 static uint32_t g_entityGenerations[ASH_MAX_ENTITIES];
 static uint8_t g_entityAlive[ASH_MAX_ENTITIES];
 
-ASH_Entity ASH_CreateEntity(void);
-void ASH_DestroyEntity(ASH_Entity entity);
+ASH_Entity ASH_CreateEntity(void)
+{
+    uint32_t index = g_entityFreelist[--g_entityFreelistCount];
+
+    ASH_Entity current_entity = {
+        .index = index,
+        .generation = g_entityGenerations[index]};
+
+    g_entityAlive[index] = 1;
+    return current_entity;
+}
+
+void ASH_DestroyEntity(ASH_Entity entity)
+{
+    g_entityFreelist[g_entityFreelistCount++] = entity.index;
+    g_entityGenerations[entity.index]++;
+    g_entityAlive[entity.index] = 0;
+}
 
 uint8_t ASH_IsEntityValid(ASH_Entity entity);
 void ASH_InitEntityPool(void);
