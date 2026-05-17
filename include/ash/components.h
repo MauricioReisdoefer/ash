1bd9eaceab1756c4aa40197b997115bb8abbb57d
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-05-17 00:17:18 -0300

    feat: starting ASH_Component

---
+++ include/ash/components.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+#include <ash/entities.h>
+
+typedef struct ASH_Component
+{
+    void *pool;
+    size_t componentSize;
+    uint8_t active;
+    uint8_t entityHasComponent[ASH_MAX_ENTITIES];
+} ASH_Component;
+
+uint32_t ASH_RegisterComponent(size_t size);
